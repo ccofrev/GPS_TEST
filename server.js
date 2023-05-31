f0f95fc3023d284927aa88c8ff9143b5f4c629d3
ccofrev <carlos.cofre@usach.cl>
2023-05-31 08:39:47 -0400

    Fix comentario

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const net = require('net');
 
 // Configuración del servidor TCP
-const HOST = '127.0.0.1'; // Escucha en todas las interfaces de red
+const HOST = '127.0.0.1'; // Escucha en local
 const PORT = 7070; // Puerto en el que se escucha
 let codComando = 109;
 
